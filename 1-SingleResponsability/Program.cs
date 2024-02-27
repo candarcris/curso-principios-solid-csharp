cf1c154ceb723a9e6568c6b4bb83171d4ab3a7e2
Candar <candarcristian@gmail.com>
2024-02-27 15:14:02 -0500

    Candar: [Add] Solutions for first and two principles

--- 1-SingleResponsability/Program.cs
+++ 1-SingleResponsability/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SingleResponsability;
 
 StudentRepository studentRepository = new();
-studentRepository.Export();
+ExportHelper exportHelper = new();
+exportHelper.ExportDataToCSV(studentRepository.GetAll());
 Console.WriteLine("Proceso Completado");
